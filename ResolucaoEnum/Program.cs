8768147b46ca18f368c6d3dc531968b8b8331b92
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-21 16:39:48 -0300

    Resolução da questão 1 - Enum

---
+++ ResolucaoEnum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ResolucaoEnum
+{
+    internal class Program
+    {
+        enum Erros
+        {
+            Ok = 200,
+            RedirecionamentoPermanente = 301,
+            RedirecionamentoTemporario = 302,
+            NaoEncontrado = 404,
+            NaoAutorizado = 405,
+            ServidorIndisponivel = 503,
+            TempoEsgotado = 504
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Erros.Ok);
+        }
+
+    }
+}
